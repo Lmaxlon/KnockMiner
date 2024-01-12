62cc8f5802e8cd9a9717311ddf864ce809340ff7
Lmaxlon <max.vuckolov@yandex.ru>
2024-01-12 20:58:36 +0300

    Генерация карты,добавление изометрии и обработчика свайпов

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -1,154 +1,146 @@
 package com.badlogic.drop;
 
-import java.util.Iterator;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Drop extends ApplicationAdapter {
-	private Texture dropImage;
-	private Texture bucketImage;
-	private Sound dropSound;
-	private Music rainMusic;
 	private SpriteBatch batch;
-
-	private BitmapFont font;
-	private OrthographicCamera camera;
-	private Rectangle bucket;
-	private Array<Rectangle> raindrops;
-	private long lastDropTime;
-
 	private Texture back;
+	private Texture welcomeBack;
+	private Texture citadel;
+	private BitmapFont font;
+	private boolean showWelcomeMessage;
+	private long startTime;
 
-	private Texture score;
+	private float mapX = 0;
+	private float mapY = 0;
 
-	public static final String FONT_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;,{}\"´`'<>";
+	private Texture cellTexture;
+	private Texture rocksTexture;
+	private int mapWidth = 100; // Ширина острова в клетках
+	private int mapHeight = 100; // Высота острова в клетках
+	private int[][] islandMap; // Карта острова, где каждое значение представляет тип клетки (например, вода, земля и т. д.)
+	private float cellSize = 200; // Размер клетки
 
 	@Override
 	public void create() {
-		new MainGame();
-		back = new Texture(Gdx.files.internal("back.png"));
-
-		//----
-		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
-
-		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-
-		// start the playback of the background music immediately
-		rainMusic.setLooping(true);
-		rainMusic.play();
-
-		// create the camera and the SpriteBatch
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
+	//	back = new Texture(Gdx.files.internal("back.png"));
+		cellTexture = new Texture(Gdx.files.internal("cell.png")); // Загрузка текстуры клетки
+		rocksTexture = new Texture(Gdx.files.internal("rocks.png"));
+		welcomeBack = new Texture(Gdx.files.internal("welcome_back.png"));
+		citadel = new Texture(Gdx.files.internal("citadel.png"));
+		font = new BitmapFont();
+		font.getData().setScale(3f);
+		font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+		showWelcomeMessage = true;
+		startTime = System.currentTimeMillis();
 
-		// create a Rectangle to logically represent the bucket
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
-		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
-		bucket.width = 64;
-		bucket.height = 64;
+		// Инициализация карты острова (здесь просто заполняем всю карту землей)
+		islandMap = new int[mapWidth][mapHeight];
+		for (int x = 0; x < mapWidth; x++) {
+			for (int y = 0; y < mapHeight; y++) {
+				islandMap[x][y] = (Math.random() < 0.175) ? 0 : 1; // Предполагаем, что 1 - это тип "rocks", 0 - это тип "grass"
+			}
+		}
+		// Проход по карте для группировки клеток типа "rocks"
+		for (int x = 0; x < mapWidth; x++) {
+			for (int y = 0; y < mapHeight; y++) {
+				if (islandMap[x][y] == 1) {
+					// Проверка соседних клеток
+					boolean hasNeighbourRock = false;
+					for (int dx = -1; dx <= 1; dx++) {
+						for (int dy = -1; dy <= 1; dy++) {
+							if (dx != 0 || dy != 0) { // Пропускаем текущую клетку
+								int nx = x + dx;
+								int ny = y + dy;
+								if (nx >= 0 && nx < mapWidth && ny >= 0 && ny < mapHeight && islandMap[nx][ny] == 1) {
+									hasNeighbourRock = true;
+									break;
+								}
+							}
+						}
+						if (hasNeighbourRock) {
+							break;
+						}
+					}
+					// Если у клетки типа "rocks" есть соседи типа "rocks", она остается типом "rocks", иначе становится типом "grass"
+					if (!hasNeighbourRock) {
+						islandMap[x][y] = 0;
+					}
+				}
+			}
+		}
 
-		// create the raindrops array and spawn the first raindrop
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
-	}
+		// Добавляем обработчик ввода для обработки свайпов
+		Gdx.input.setInputProcessor(new InputAdapter() {
+			private float lastX;
+			private float lastY;
 
-	private void spawnRaindrop() {
-		Rectangle raindrop = new Rectangle();
-		raindrop.x = MathUtils.random(0, 800-64);
-		raindrop.y = 480;
-		raindrop.width = 64;
-		raindrop.height = 64;
-		raindrops.add(raindrop);
-		lastDropTime = TimeUtils.nanoTime();
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				lastX = screenX;
+				lastY = screenY;
+				return true;
+			}
+
+			@Override
+			public boolean touchDragged(int screenX, int screenY, int pointer) {
+				float deltaX = screenX - lastX;
+				float deltaY = screenY - lastY;
+				mapX += deltaX;
+				mapY -= deltaY; // Инвертируем направление движения по Y, так как Y растет вниз
+				lastX = screenX;
+				lastY = screenY;
+				return true;
+			}
+		});
 	}
 
 	@Override
 	public void render() {
-		// clear the screen with a dark blue color. The
-		// arguments to clear are the red, green
-		// blue and alpha component in the range [0,1]
-		// of the color to be used to clear the screen.
-		ScreenUtils.clear(0, 0, 0.2f, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		// tell the camera to update its matrices.
-		camera.update();
-
-		// tell the SpriteBatch to render in the
-		// coordinate system specified by the camera.
-		batch.setProjectionMatrix(camera.combined);
-
-		// begin a new batch and draw the bucket and
-		// all drops
-		batch.begin();
-		batch.draw(back, 0, 0, 800, 600);
-	//	font.draw(batch, "Hello World!", 10, 10);
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for(Rectangle raindrop: raindrops) {
-			batch.draw(dropImage, raindrop.x, raindrop.y);
-		}
-		batch.end();
-
-		// process user input
-		if(Gdx.input.isTouched()) {
-			Vector3 touchPos = new Vector3();
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			camera.unproject(touchPos);
-			bucket.x = touchPos.x - 64 / 2;
-		}
-		if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-
-		// make sure the bucket stays within the screen bounds
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-
-		// check if we need to create a new raindrop
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-
-		// move the raindrops, remove any that are beneath the bottom edge of
-		// the screen or that hit the bucket. In the latter case we play back
-		// a sound effect as well.
-		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
-			Rectangle raindrop = iter.next();
-			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if(raindrop.y + 64 < 0) iter.remove();
-			if(raindrop.overlaps(bucket)) {
-				dropSound.play();
-				iter.remove();
+		if (showWelcomeMessage && System.currentTimeMillis() - startTime < 5000) {
+			batch.begin();
+			batch.draw(welcomeBack, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			font.draw(batch, "Welcome to the Game!", Gdx.graphics.getWidth() / 2 - 200, Gdx.graphics.getHeight() / 2 - 200);
+			batch.end();
+		} else {
+			showWelcomeMessage = false;
+			batch.begin();
+			// Отрисовка острова из клеток
+			for (int x = 0; x < mapWidth; x++) {
+				for (int y = 0; y < mapHeight; y++) {
+					float offsetX = (y % 2 == 0) ? 0 : cellSize / 2; // Смещение для каждой второй строки
+					if (islandMap[x][y] == 1) {
+						batch.draw(cellTexture, x * cellSize * 0.75f + offsetX + mapX, y * cellSize * 0.5f + mapY, cellSize, cellSize);
+					}
+					if (islandMap[x][y] == 0) {
+						batch.draw(rocksTexture, x * cellSize * 0.75f + offsetX + mapX, y * cellSize * 0.5f + mapY, cellSize, cellSize);
+					}
+				}
 			}
+			float centerX = 5 * cellSize * 0.75f + ((5 % 2 == 0) ? 0 : cellSize / 2) + mapX;
+			float centerY = cellSize * 0.5f + mapY;;
+			batch.draw(citadel, centerX, centerY, citadel.getWidth() / 3, citadel.getHeight() / 3);
+			font.draw(batch, "Pre-alpha version", Gdx.graphics.getWidth() - 400, Gdx.graphics.getHeight() - 50);
+			batch.end();
 		}
 	}
 
 	@Override
 	public void dispose() {
-		// dispose of all the native resources
-		dropImage.dispose();
-		bucketImage.dispose();
-		dropSound.dispose();
-		rainMusic.dispose();
 		batch.dispose();
+		cellTexture.dispose();
+		welcomeBack.dispose();
+		citadel.dispose();
+		font.dispose();
 	}
 }
